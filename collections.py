2eba0462390b86fb7462fb2d511027e1ec984e0d
Mohab Khalifa <khalifamohab97@gmail.com>
2021-10-04 00:12:57 +0100

    creating lists in python

---
+++ collections.py
@@ -0,0 +1,26 @@
+varOfShopping = ['meat', 'veg', 'cake', 'milk' ]
+
+print (varOfShopping)
+
+print (varOfShopping[1])
+
+print (varOfShopping[0])
+
+varForAString = 'I like pies'
+
+print(varForAString[4])
+
+
+
+
+cool_cows = ["Winnie the Moo", "Moolan", "Milkshake", "Mooana"]
+cool_sheep = ["Baaaart", "Baaaarnaby"]
+cool_pigs = ["Chris P. Bacon", "Hamlet", "Hogwarts"]
+
+cool_animals = [cool_cows, cool_sheep, cool_pigs]
+
+print (cool_animals)
+
+print (cool_animals[1])
+
+print (cool_animals[2][1])
